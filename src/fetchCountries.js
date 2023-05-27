5d478db076acf8736a104aef47b556dd7cbbabed
Sokolova-LV <leyla-@ukr.net>
2023-05-27 11:53:01 +0300

    changed

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,6 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name/';
 
-const fields = '?fields=capital,population,flags,languages';
+const fields = '?fields=name,capital,population,flags,languages';
 
 function fetchCountries(name) {
     return fetch(`${BASE_URL}${name}${fields}`)
